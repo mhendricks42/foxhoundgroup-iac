b43f5ad65ac0dd5efb68ad364c27285a4da54436
mh <mh@foxhoundgroup.org>
2023-03-13 22:08:49 -0400

    lin vs win fix

--- fhg-enterprise/staging/eastus/vm-ubuntu-server/terragrunt.hcl
+++ fhg-enterprise/staging/eastus/vm-ubuntu-server/terragrunt.hcl
@@ -1,5 +1,5 @@
 terraform {
-  source = "${get_parent_terragrunt_dir()}/modules//azure-vm"
+  source = "${get_parent_terragrunt_dir()}/modules//azure-vm-linux"
 }
 
 include {
---
+++ modules/azure-vm-linux/main.tf
@@ -0,0 +1,60 @@
+# Public IP address
+resource "azurerm_public_ip" "example" {
+  name                = var.public_ip_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+# Network interface
+resource "azurerm_network_interface" "example" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = var.subnet_id
+    public_ip_address_id          = azurerm_public_ip.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Virtual machine
+resource "azurerm_virtual_machine" "example" {
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  vm_size               = var.vm_size
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
+  
+  storage_os_disk {
+    name              = var.os_disk_name
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
+}
+
+
